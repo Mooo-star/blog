cc7cf9fd7bd0ca63a7b810e6c646abe454e3f29c
sunny <laomo202012@163.com>
2024-07-17 19:57:27 +0800

    feat: node 模块, fs 文件系统

--- config/nav/index.ts
+++ config/nav/index.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'dumi';
 import engineer from './engineer';
 import fragment from './fragment';
+import node from './node';
 import react from './react';
 
 type Nav = ReturnType<typeof defineConfig>;
 
-const nav: Nav = [fragment, engineer, react];
+const nav: Nav = [fragment, engineer, react, node];
 
 export default nav;
---
+++ config/nav/node/index.ts
@@ -0,0 +1,9 @@
+export default {
+  title: 'Node',
+  children: [
+    {
+      title: 'fs',
+      link: '/node/fs',
+    },
+  ],
+};
