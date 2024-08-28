74c1ab64544f4e5f48b18c355adfa4bb0156263e
sunny <laomo202012@163.com>
2024-08-28 19:39:37 +0800

    feat: http 各版本差异

--- config/routes/fragment/net.ts
+++ config/routes/fragment/net.ts
@@ -4,6 +4,10 @@ export default [
     link: '/net/http_cache_proto',
   },
   {
+    title: 'HTTP 各版本差异',
+    link: '/net/http_version_diff',
+  },
+  {
     title: '网络相关文章参考',
     link: '/net/forward',
   },
